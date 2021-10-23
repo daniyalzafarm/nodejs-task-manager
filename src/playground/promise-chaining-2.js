e775b364ae4b445814a524f4266707f8542374a8
Muhammad Daniyal Zafar <daniyalzafar836@gmail.com>
2021-10-23 22:54:21 +0500

    Using async-await with Promises

--- src/playground/promise-chaining-2.js
+++ src/playground/promise-chaining-2.js
@@ -1,13 +1,28 @@
 const mongoose = require("../db/mongoose");
 const Task = require("../models/Task");
+const User = require("../models/User");
 
-Task.findByIdAndDelete("61729d2451834344dba448da")
-  .then((result) => {
-    console.log(result);
-    return Task.countDocuments({ completedStatus: false });
-  })
+// Task.findByIdAndDelete("61729d2451834344dba448da")
+//   .then((result) => {
+//     console.log(result);
+//     return Task.countDocuments({ completedStatus: false });
+//   })
+//   .then((count) => {
+//     console.log("UnCompleted Tasks: ", count);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+const deleteTaskAndCount = async (id) => {
+  const deleteTask = await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completedStatus: false });
+  return count;
+};
+
+deleteTaskAndCount("61732d021997be00fa25d2bc")
   .then((count) => {
-    console.log("UnCompleted Tasks: ", count);
+    console.log(count);
   })
   .catch((e) => {
     console.log(e);
--- src/playground/promise-chaining.js
+++ src/playground/promise-chaining.js
@@ -1,11 +1,25 @@
 const mongoose = require("../db/mongoose");
 const User = require("../models/User");
 
-User.findByIdAndUpdate("6173273cce1ae81441ed5828", { age: 22 })
-  .then((user) => {
-    console.log(user);
-    return User.countDocuments({ age: 22 });
-  })
+// User.findByIdAndUpdate("6173273cce1ae81441ed5828", { age: 22 })
+//   .then((user) => {
+//     console.log(user);
+//     return User.countDocuments({ age: 22 });
+//   })
+//   .then((count) => {
+//     console.log(count);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age });
+  const count = await User.countDocuments({ age });
+  return count;
+};
+
+updateAgeAndCount("6173273cce1ae81441ed5828", 21)
   .then((count) => {
     console.log(count);
   })
