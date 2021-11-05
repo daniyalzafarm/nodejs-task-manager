c2dac6f488a32a6028d35fe5d2a833250d3df3ff
Muhammad Daniyal Zafar <daniyalzafar836@gmail.com>
2021-11-05 23:30:14 +0500

    Update, Delete and Routers

--- src/index.js
+++ src/index.js
@@ -1,138 +1,15 @@
 const express = require("express");
 require("./db/mongoose");
-const User = require("./models/User");
 const Task = require("./models/Task");
+const userRouter = require("./routers/userR");
+const taskRouter = require("./routers/taskR");
 
 const app = express();
 const port = process.env.port || 3000;
 
 app.use(express.json());
-
-// Creating User
-app.post("/users", async (req, res) => {
-  const user = new User(req.body);
-
-  try {
-    await user.save();
-    res.status(201).send(user);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-
-  // user
-  //   .save()
-  //   .then(() => {
-  //     console.log(user);
-  //     res.status(201).send(user);
-  //   })
-  //   .catch((e) => {
-  //     res.status(400).send(e);
-  //   });
-});
-
-// Reading all Users
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-  // User.find({})
-  //   .then((users) => {
-  //     res.send(users);
-  //   })
-  //   .catch((e) => {
-  //     res.status(500).send(e);
-  //   });
-});
-
-// Read specific User by id
-app.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-  // User.findById(_id)
-  //   .then((user) => {
-  //     if (!user) {
-  //       res.status(404).send();
-  //     }
-  //     res.send(user);
-  //   })
-  //   .catch((e) => {
-  //     res.status(404).send(e);
-  //   });
-});
-
-// Create Task
-app.post("/tasks", async (req, res) => {
-  const task = new Task(req.body);
-
-  try {
-    await task.save();
-    res.send(task);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-  // task
-  //   .save()
-  //   .then(() => {
-  //     console.log(task);
-  //     res.status(201).send(task);
-  //   })
-  //   .catch((e) => {
-  //     res.status(400).send(e);
-  //   });
-});
-
-// Read all Tasks
-app.get("/tasks", async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.send(tasks);
-  } catch (error) {
-    res.status(500).send(e);
-  }
-  // Task.find({})
-  //   .then((tasks) => {
-  //     res.send(tasks);
-  //   })
-  //   .catch((e) => {
-  //     res.status(500).send(e);
-  //   });
-});
-
-// Read Task by Id
-app.get("/tasks/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const task = await Task.findById(_id);
-    if (!task) {
-      return res.status(404).send();
-    }
-    res.send(task);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-  // Task.findById(_id)
-  //   .then((task) => {
-  //     if (!task) {
-  //       res.status(404).send();
-  //     }
-  //     res.send(task);
-  //   })
-  //   .catch((e) => {
-  //     res.status(500).send(e);
-  //   });
-});
+app.use(userRouter);
+app.use(taskRouter);
 
 // Run Express Server
 app.listen(port, () => {
---
+++ src/routers/userR.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const User = require("../models/User");
+const router = new express.Router();
+
+// Creating User
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+
+  // user
+  //   .save()
+  //   .then(() => {
+  //     console.log(user);
+  //     res.status(201).send(user);
+  //   })
+  //   .catch((e) => {
+  //     res.status(400).send(e);
+  //   });
+});
+
+// Reading all Users
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+  // User.find({})
+  //   .then((users) => {
+  //     res.send(users);
+  //   })
+  //   .catch((e) => {
+  //     res.status(500).send(e);
+  //   });
+});
+
+// Read specific User by id
+router.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+  // User.findById(_id)
+  //   .then((user) => {
+  //     if (!user) {
+  //       res.status(404).send();
+  //     }
+  //     res.send(user);
+  //   })
+  //   .catch((e) => {
+  //     res.status(500).send(e);
+  //   });
+});
+
+// Update User
+router.patch("/users/:id", async (req, res) => {
+  // Validating Update Properties
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidUpdate = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidUpdate) {
+    return res.status(404).send({ error: "Invalid Updates!" });
+  }
+
+  try {
+    const user = await User.findOneAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Delete User
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
