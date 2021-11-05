f2b76a72ac2118d8c65a2ac2b89e1485c2bef590
Muhammad Daniyal Zafar <daniyalzafar836@gmail.com>
2021-11-05 19:16:21 +0500

    Revisio/*

--- src/playground/promise-chaining-2.js
+++ src/playground/promise-chaining-2.js
@@ -2,6 +2,15 @@ const mongoose = require("../db/mongoose");
 const Task = require("../models/Task");
 const User = require("../models/User");
 
+/*
+note: 
+
+-- Promise chaning is used when we need to Re-Process response of fisrt promise.
+
+-- To Use promise chaining, return the second promise in then() of first promise And agai Use then() to process the second Promise, After handaling all promises use a single cath Statement to Handle errors.
+
+*/
+
 // Task.findByIdAndDelete("61729d2451834344dba448da")
 //   .then((result) => {
 //     console.log(result);
@@ -14,6 +23,15 @@ const User = require("../models/User");
 //     console.log(e);
 //   });
 
+/*
+note: 
+
+-- async function always return a promise( having the returned Value ).
+-- after creating the funtion, we use it as a Promise and process then() & catch() block.
+-- async function allow us to access all values in a single scope.
+-- we use and await promise returning funtions in async funtion.
+
+*/
 const deleteTaskAndCount = async (id) => {
   const deleteTask = await Task.findByIdAndDelete(id);
   const count = await Task.countDocuments({ completedStatus: false });
--- src/playground/promise-chaining.js
+++ src/playground/promise-chaining.js
@@ -1,6 +1,15 @@
 const mongoose = require("../db/mongoose");
 const User = require("../models/User");
 
+/*
+note: 
+
+-- Promise chaning is used when we need to Re-Process response of fisrt promise.
+
+-- To Use promise chaining, return the second promise in then() of first promise And agai Use then() to process the second Promise, After handaling all promises use a single cath Statement to Handle errors.
+
+*/
+
 // User.findByIdAndUpdate("6173273cce1ae81441ed5828", { age: 22 })
 //   .then((user) => {
 //     console.log(user);
@@ -13,6 +22,15 @@ const User = require("../models/User");
 //     console.log(e);
 //   });
 
+/*
+note: 
+
+-- async function always return a promise( having the returned Value ).
+-- after creating the funtion, we use it as a Promise and process then() & catch() block.
+-- async function allow us to access all values in a single scope.
+-- we use and await promise returning funtions in async funtion.
+
+*/
 const updateAgeAndCount = async (id, age) => {
   const user = await User.findByIdAndUpdate(id, { age });
   const count = await User.countDocuments({ age });
